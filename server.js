61fff7b8f2ebfdb0499441fdda58d187c3fd3a41
Vitor Machado Silva <vitor.machado.silva05@gmail.com>
2024-11-05 17:04:34 -0300

    final

--- server.js
+++ server.js
@@ -1,29 +1,71 @@
 import express from 'express' //importando biblioteca
+import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
+
+const prisma = new PrismaClient()
 
 const app = express() //inicializando("documentado")
-app.use(express.json)//avisar express para usar json
-const user = []
 
-app.post('/usuarios',(req, res)=>{
-     console.log(req)
-     res.send('Ok, deu bom!')
+app.use(express.json())//avisar express para usar json
+app.use(cors())
+
+app.post('/usuarios', async (req, res)=>{
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+     res.status(201).json(req.body)
 })
 
-/*
-==================Rotas==============
-1) tipo de rota / método
-2) endereço ("www.google/buscar/.com")
-*/
-app.get('/usuarios', (req, res)=>{
-    res.send('Ok, deu bom!')//enviando resposta
+app.get('/usuarios', async (req, res)=>{
+    let usuarios = []
+    if (req.query){
+        usuarios = await prisma.user.findMany({
+            where:{
+                name: req.query.name
+            }
+        })
+    }else{
+        usuarios = await prisma.user.findMany()
+    }
+    const users = await prisma.user.findMany()
+    res.status(200).json(users)
 })
-/*
-Criar listagem de usuarios(API)
 
-    -criar um usuario
-    -listar todos os usuarios
-    -editar usuario
-    -deletar usuario
-*/
+
+app.put('/usuarios/:id', async (req, res)=>{
+    await prisma.user.update({
+        where:{
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+     res.status(201).json(req.body)
+})
+
+
+app.delete('/usuarios/:id', async (req, res)=>{
+    await prisma.user.delete({
+        where:{
+            id: req.params.id
+        }
+    })
+     res.status(201).send("Deletado com sucesso")
+})
+
 
 app.listen(7000)
+
+/*
+vitor
+iHQUyHLtoqqaZpIY
+*/
